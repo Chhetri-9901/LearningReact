61c3ba00f46cfafe9a224eb47048e3d26dd82a0b
Chhetri-9901 <vivekchhetri9901@gmail.com>
2026-06-21 20:33:23 +0530

    subscription WIP

--- React Projects/Life OS/life-os/src/components/Sidebar.jsx
+++ React Projects/Life OS/life-os/src/components/Sidebar.jsx
@@ -3,37 +3,61 @@ import compressArrow from "../assets/arrows.png";
 import expandArrow from "../assets/next.png";
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
 
-  const [isOpen, setIsOpen] = useState(true)
+  function getTime() {
+    let hours = new Date().getHours();
+    if(hours >=0 && hours <12) {
+      return("Good morning");
+    }
+    else if(hours >=12 && hours <5) {
+      return ("Good afternoon");
+    }
+    else {
+      return ("Good evening");
+    }
+  }
 
-  if(!isOpen) {
+  if (!isOpen) {
     return (
       <div className="w-2/16 h-[100vh] bg-[var(--dark-blue)] px-6 py-8 sidebar">
-      <div className="flex justify-between gap-4 items-center min-h-10">
-        <h1 className="text-white text-xl">Life OS</h1>
-        <button
-          type="button"
-          className="text-white font-semibold text-lg cursor-pointer close w-[100%] max-w-8"
-          onClick={() => {setIsOpen(true)}}
-        >
-          <img src={expandArrow} alt="expand side bar" className="w-[100%]" />
-        </button>
+        <div className="flex justify-between gap-4 items-center min-h-10">
+          <h1 className="text-white text-xl">Life OS</h1>
+          <button
+            type="button"
+            className="text-white font-semibold text-lg cursor-pointer close w-[100%] max-w-8"
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          >
+            <img src={expandArrow} alt="expand side bar" className="w-[100%]" />
+          </button>
+        </div>
       </div>
-    </div>
     );
   }
 
   return (
-    <div className="w-2/8 h-[100vh] bg-[var(--dark-blue)] px-6 py-8 sidebar">
+    <div className="w-2/8 h-[100vh] bg-[var(--dark-blue)] px-6 py-8 sidebar flex flex-col justify-between">
       <div className="flex justify-between items-center min-h-10">
         <h1 className="text-white text-xl">Life OS</h1>
         <button
           type="button"
           className="text-white font-semibold text-lg cursor-pointer close"
-          onClick={() => {setIsOpen(false)}}
+          onClick={() => {
+            setIsOpen(false);
+          }}
         >
-          <img src={compressArrow} alt="compress side bar" className="w-full max-w-[40px]" />
+          <img
+            src={compressArrow}
+            alt="compress side bar"
+            className="w-full max-w-[40px]"
+          />
         </button>
+      </div>
+
+      <div>
+        <p className="text-2xl font-bold text-zinc-50">{getTime()}</p>
       </div>
     </div>
   );
